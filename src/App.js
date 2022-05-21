5ea849ccf57173bce9abf2c3a3c9bce7bf1682fe
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-05-21 17:18:22 -0300

    1010101010

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
-import Blog from "./Blog";
+import Blog from "./routes/Blog";
+import Error from "./routes/Error";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/undefined" element={<Error />} />
       </Routes>
     </>
   );
