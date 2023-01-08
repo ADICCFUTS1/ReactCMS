f3608faf5e73e7dd3e6c596dc85d86874bc3a82c
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2023-01-08 00:10:57 -0300

    10101010101010101010

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import Blog from "./routes/Blog";
 import Estadisticas from "./routes/Blog2";
+import Error from "./routes/Error";
 
 function Apps() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Error />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/estadisticas" element={<Estadisticas />} />
       </Routes>
